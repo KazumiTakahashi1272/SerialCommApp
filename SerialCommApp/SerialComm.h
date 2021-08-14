ffe501458a83488f9bc2b71fc40bbe649f3ccf76
Takahashi Kazumi <kazumit@384.jp>
2021-08-14 11:43:40 +0900

    TTYInfoの定義

--- SerialCommApp/SerialComm.h
+++ SerialCommApp/SerialComm.h
@@ -19,12 +19,72 @@ using namespace std;
 #pragma message("Automatically linking with SerialComm.dll")
 #endif
 
+//
+// hard coded maximum number of ports
+//
+#define MAXPORTS        10
+
+//
+// terminal size
+//
+#define MAXROWS         50
+#define MAXCOLS         80
+
+//
+// cursor states
+//
+#define CS_HIDE         0x00
+#define CS_SHOW         0x01
+
+//
+// ascii definitions
+//
+#define ASCII_BEL       0x07
+#define ASCII_BS        0x08
+#define ASCII_LF        0x0A
+#define ASCII_CR        0x0D
+#define ASCII_XON       0x11
+#define ASCII_XOFF      0x13
+
 //----------------------------------------------------------------------------
-//	暗号化パラメータ
+// 通信パラメータ
 //----------------------------------------------------------------------------
 typedef struct _SERIAL_DATA
 {
 } T_SERIAL_DATA;
+
+//
+// data 構造体
+//
+struct TTYInfoStruct
+{
+    HANDLE  hCommPort, hReaderStatus, hWriter ;
+    DWORD   dwEventFlags;
+    CHAR    Screen[MAXCOLS * MAXROWS];
+    CHAR    chFlag, chXON, chXOFF;
+    WORD    wXONLimit, wXOFFLimit;
+    DWORD   fRtsControl;
+    DWORD   fDtrControl;
+    BOOL    fConnected, fTransferring, fRepeating,
+            fLocalEcho, fNewLine,
+            fDisplayErrors, fAutowrap,
+            fCTSOutFlow, fDSROutFlow, fDSRInFlow, 
+            fXonXoffOutFlow, fXonXoffInFlow,
+            fTXafterXoffSent,
+            fNoReading, fNoWriting, fNoEvents, fNoStatus,
+            fDisplayTimeouts;
+    BYTE    bPort, bByteSize, bParity, bStopBits ;
+    DWORD   dwBaudRate ;
+    WORD    wCursorState ;
+    HFONT   hTTYFont ;
+    LOGFONT lfTTYFont ;
+    DWORD   rgbFGColor ;
+    COMMTIMEOUTS timeoutsorig;
+    COMMTIMEOUTS timeoutsnew;
+    int     xSize, ySize, xScroll, yScroll, xOffset, yOffset,
+            nColumn, nRow, xChar, yChar , nCharPos;
+
+} TTYInfo;
 
 
 #ifdef __cplusplus
